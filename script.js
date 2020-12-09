058c8324cc03cf6edbddc587cd64c650e2c02e0a
arianna choza <unachoza@gmail.com>
2020-12-09 15:57:27 -0800

    dom elements created for play grids

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
-// alert('linked');
+//make dom elements
+document.addEventListener('DOMContentLoaded', () => {
+  const userGrid = document.querySelector('.grid-user');
+  const computerGrid = document.querySelector('.grid-computer');
+  const displayGrid = document.querySelector('.display-user');
+  const ships = document.querySelectorAll('.ships');
+  const destroyer = document.querySelector('.destroyer-container');
+  const submarine = document.querySelector('.submarine-container');
+  const cruiser = document.querySelector('.cruiser-container');
+  const battleship = document.querySelector('.battleship-container');
+  const carrier = document.querySelector('.carrier-container');
+  const startButton = document.querySelector('#start-button');
+  const rotateButton = document.querySelector('#rotate-button');
+  const turnDisplay = document.querySelector('#whose-go');
+  const infoDisplay = document.querySelector('#info');
+  const userSquares = [];
+  const computerSquares = [];
+
+  const width = 10;
+
+  // Create Board
+
+  const createBoard = (grid, squares) => {
+    for (let i = 0; i < width * width; i++) {
+      const square = document.createElement('div');
+      square.dataset.id = i;
+      grid.appendChild(square);
+      squares.push(square);
+    }
+  };
+
+  createBoard(userGrid, userSquares);
+  createBoard(computerGrid, computerSquares);
+
+  // createBoard(userGrid, userSquares, width);
+});
